088e2fb3697d73c80abb5d12bd738144d41502f8
Murilo Varoto <muriloalves121@Outlook.com>
2024-06-09 12:24:59 -0300

    video 1 concluido, seletiva de dados bruto

--- src/script.js
+++ src/script.js
@@ -7,12 +7,19 @@ const fetchApi = (value) => {
     const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data.name)
-    });
+        console.log(data)
+        return data; 
+    })
 
-
+    return result;
 }
-fetchApi(1)
-btnGo.addEventListener('click', () => {
 
-});
+btnGo.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const result = await fetchApi(characterId.value)
+
+    content.textContent  = `${JSON.stringify(result, undefined, 2)}`;
+
+    image.src = `${result.image}`
+}) 
+
